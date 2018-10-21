c688bf647a9e9775b98b59a91a8654a00520588a
Stephen Allatt <stephen.allatt@gmail.com>
2018-10-21 21:35:34 +0100

    finish lexer and repl, end chapter 1

---
+++ README.md
@@ -0,0 +1,6 @@
+# interpreter
+Code going through '_Writing An Interpreter In Go_' by _Thorsten Ball_
+
+https://www.amazon.co.uk/Writing-Interpreter-Go-Thorsten-Ball/dp/3982016118/
+
+https://interpreterbook.com/
--- token/token.go
+++ token/token.go
@@ -3,7 +3,7 @@ package token
 type TokenType string
 
 type Token struct {
-	Type TokenType
+	Type    TokenType
 	Literal string
 }
 
@@ -18,6 +18,15 @@ const (
 	// Operators
 	ASSIGN   = "="
 	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+	EQ       = "=="
+	NOT_EQ   = "!="
+
+	LT = "<"
+	GT = ">"
 
 	// Delimiters
 	COMMA     = ","
@@ -31,4 +40,26 @@ const (
 	// Keywords
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
 )
+
+var keywords = map[string]TokenType{
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
